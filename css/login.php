d62c5d90bce9a1fec54f569944009ef94872e232
Francisco:git config --global user.name Franciscogit config --global user.name Francisco <a335050@uabc.edu.mx>
2017-05-24 03:24:47 -0700

    Actualizando proyecto petme

---
+++ css/login.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+?>
+
+<?php require_once("includes/connection.php"); ?>
+
+<?php
+
+if(isset($_SESSION["session_username"])){
+// echo "Session is set"; // for testing purposes
+header("Location: index.php");
+}
+
+if(isset($_POST["login"])){
+
+if(!empty($_POST['username']) && !empty($_POST['password'])) {
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
+    $peticion = "SELECT * FROM usuarios WHERE correo='".$username."' AND password='".$password."'";
+
+    $query =mysql_query($peticion);
+
+    $numrows=mysql_num_rows($query);
+
+    if($numrows!=0)
+    {
+        while($row=mysql_fetch_assoc($query))
+        {
+        $dbusername=$row['correo'];
+        $dbpassword=$row['password'];
+        $dbnombre=$row['nombre'];
+    }
+
+    if($username == $dbusername && $password == $dbpassword)
+
+    {
+
+
+    $_SESSION['session_username']=$dbnombre;
+
+    /* Redirect browser */
+    header("Location: index.php");
+    }
+    } else {
+
+ $message =  "Nombre de usuario ó contraseña invalida!";
+    }
+
+} else {
+    $message = "Todos los campos son requeridos!";
+}
+}
+?>
+  
+  <title>PetMe: Iniciar Sesion</title>
+<?php include("includes/header.php"); ?>
+<link href="css/freelancer.min.css" rel="stylesheet">
+
+<div class="container mlogin login">
+    <div id="login">
+             <img src="img/logo1.png">
+        <h1>Iniciar Sesion</h1>
+
+        <form name="loginform" id="loginform" action="" method="POST">
+            <p>
+            <input type="email" name="username" id="username" placeholder="Correo Electronico" class="input" required="requiered" /></label>
+            </p>
+            
+            <p>
+            <input placeholder="Contraseña" type="password" name="password" id="password" class="input" requiered="requiered" /></label>
+            </p>
+
+            <p class="submit">
+            <input type="submit" name="login" class="button btn btn-primary btn-block btn-large" value="Entrar" />
+            </p>
+
+            <p class="regtext">No estas registrado? <a href="register.php" >Registrate Aquí</a>!</p>
+
+        </form>
+
+    </div>
+
+</div>
+	
+	<?php if (!empty($message)) {echo "<p class=\"error\">" . "MESSAGE: ". $message . "</p>";} ?>
+	
---
+++ registroMascota.php
@@ -0,0 +1,163 @@
+<?php require_once("includes/connection.php"); ?>
+<?php include("includes/header.php"); ?>
+
+<title>PetMe: Registrar Mascota</title>
+
+<?php
+    if(isset($_POST["enviar"])){
+
+        if (!empty($_POST['titulo']) && !empty($_POST['categoria']) && !empty($_POST['edad']) && !empty($_POST['lugar']) && !empty($_POST['descripcion']) && !empty($_POST['image'])){
+            $titulo=$_POST['titulo'];
+            $categoria=$_POST['categoria'];
+            $edad=$_POST['edad'];
+            $image=$_POST['image'];
+            $lugar=$_POST['lugar'];
+            $descripcion=$_POST['descripcion'];
+            $hoy = getdate();
+
+            $sql = "INSERT INTO usuarios(titulo, descripcion, categoria, edad_mascota, lugar, fecha, foto) VALUES('$titulo','$descripcion','$categoria','$edad','$lugar','$hoy','$image')";
+
+            $resultQuery = mysql_query($sql);
+
+            if ($resultQuery){
+                $message = "La mascota ha sido registrada con exito!";
+            }else{
+                $message = "Error al registrar la mascota!";
+            }
+        } else {
+            $message = "Se omitio un campo obligatorio!";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Modal</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+
+<body>
+
+
+<!-- Trigger -->
+<div class="boton" onclick="desplegar()">
+    <div class="txt">
+        Registrar Mascota
+    </div>
+
+</div>
+<br>
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+
+        <div class="modal-header">
+            <span class="close">&times;</span>
+            <h2>Registrar mascota</h2>
+        </div>
+
+
+        <div class="modal-body">
+            <!-- Show feedback sent -->
+            <div class="result">
+                Registrado correctamente
+            </div>
+
+            <!-- form -->
+            <div class="form">
+
+                <!-- <form action="" -->
+                <form id="formulario" name="form" method="post" enctype="multipart/form-data" action="">
+
+
+                    <!-- Title -->
+                    <label for="titulo">Titulo</label>
+                    <input type="text" id="titulo" maxlength="30" name="titulo" value="">
+
+
+                    <label for="categoria">Categoria</label>
+                    <select id="categoria" name="categoria">
+                        <option value="Perros" name="categoria">Perros</option>
+                        <option value="Gatos" name="categoria">Gatos</option>
+                    </select>
+
+                    <!-- Tel num  -->
+                    <label for="edad">Edad</label>
+                    <input type="text" id="edad" name="edad" placeholder="Años de la mascota">
+
+                    <!-- Fecha
+                    <label for="date">Fecha</label>
+                    <input type="text" readonly="readonly" name="date" id="date" value="">
+                    -->
+
+                    <!-- Part num
+                    <label for="numeroParte">Numero de Parte</label>
+                    <input type="text" readonly="readonly" id="numeroParte" name="numeroParte" value="%%GLOBAL_SKU%%"
+                           autocomplete="off" class="txtinput">
+                    -->
+
+                    <!--
+                    <label for="quantity">Cantidad</label>
+                    <input type="number" id="quantity" name="quantity" min="1" max="50" value="1">
+                    -->
+
+
+                    <!-- URL -->
+                    <input type="hidden" name="url" id="url" value="">
+
+                    <label for="lugar">Lugar</label>
+                    <select id="lugar" name="lugar">
+                        <option value="Ensenada" name="lugar">Ensenada</option>
+                        <option value="Tecate" name="lugar">Tecate</option>
+                        <option value="Tijuana" name="lugar">Tijuana</option>
+                    </select>
+                    <br>
+                    <!-- Comment  -->
+                    <label for="descripcion">Descripción</label>
+                    <div class="descripcion">
+                        <textarea name="descripcion" id="descripcion" placeholder="Describe a la mascota" class="texto"
+                                  rows="5"></textarea>
+                    </div>
+
+                    <!-- Subir imagen -->
+                    <input type="file" name="image" id="image">
+
+
+                    <!-- Captcha -->
+                    <div class="captcha">
+                        <div class="g-recaptcha" data-sitekey=""
+                             data-callback="YourOnSubmitFn"></div>
+                    </div>
+
+                    <!-- Submit -->
+                    <div class="sub">
+                        <input type="submit" id="enviar" name="enviar" value="Registrar">
+                    </div>
+
+
+                </form>
+
+            </div>
+        </div>
+
+
+        <div class="modal-footer">
+            <h3></h3>
+        </div>
+
+    </div>
+
+</div>
+
+
+<script src='js/myScript.js' charset="utf-8"></script>
+
+</body>
+
+</html>
